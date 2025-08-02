f3ae7730765f97abf14b508d6ca5e0994a53e7b7
Dalmas Otieno <dalmasotieno@Dalmass-MacBook-Air.local>
2025-08-03 00:30:05 +0300

    collect data

---
+++ data/scrape_data_links.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+class ScrapeDataLinks:
+    def __init__(self):
+        self.host = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
+        self.dataset = []
+
+
+    def read_links(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(self.host)
+        expand_all_button = driver.find_element(By.CLASS_NAME, "faq-expand-all")
+        expand_all_button.click()
+
+        freq_ans_divs = driver.find_elements(By.CLASS_NAME, "faq-answers")
+        freq_qtns_divs = driver.find_elements(By.CLASS_NAME, "faq-questions")
+
+        n = 0
+        for freq_ans_div, freq_qtns_div in zip(freq_ans_divs, freq_qtns_divs):
+            n += 1
+            year = freq_qtns_div.find_element(By.TAG_NAME, "p").text
+            table = freq_ans_div.find_element(By.TAG_NAME, "table")
+            cells = table.find_elements(By.TAG_NAME, "td")
+            for cell in cells:
+                months = cell.find_elements(By.TAG_NAME, "strong")
+                month_data_lists = cell.find_elements(By.TAG_NAME, "ul")
+                for month, data_list in zip(months, month_data_lists):
+                    month_link_list = data_list.find_elements(By.TAG_NAME, "li")
+                    link_list = [
+                        {
+                            "year": year,
+                            "month": month.text,
+                            "title": link_tag.find_element(By.TAG_NAME, "a").text,
+                            "link": link_tag.find_element(
+                                By.TAG_NAME, "a"
+                            ).get_property("href"),
+                        }
+                        for link_tag in month_link_list
+                    ]
+                    for obj in link_list:
+                        self.dataset.append(obj)
+        self.df = pd.DataFrame(self.dataset)
+
+
+if __name__ == "__main__":
+    scraper = ScrapeDataLinks()
+    scraper.read_links()
+    scraper.df.to_csv('data/data.csv', index=False)
---
+++ libs/write_to_s3.py
@@ -0,0 +1,4 @@
+
+class WriteToS3:
+    def __init__(self):
+        pass
